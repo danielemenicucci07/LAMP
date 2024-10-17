0b1c9759a73b9733197dfd62a011687d3cf41495
ciao mondo <danielemenicucci3@gmail.com>
2024-10-17 07:28:12 +0000

    commit

--- www/ES02/index_get_es02a.php
+++ www/ES02/index_get_es02a.php
@@ -11,11 +11,11 @@
 
 
         <label for="username">Username:</label>
-        <input type="text" name="username" id="username">
+        <input type="text" name="username" id="daniele">
         <br>
 
         <label for="password">Password:</label>
-        <input type="password" name="password" id="password">
+        <input type="password" name="password" id="Forzainter">
         <br>
         <input type="submit" value="Accedi">
         <input type="reset" value="Annulla">
