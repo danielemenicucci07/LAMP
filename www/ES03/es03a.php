97f20e4dff7ef9a01ce06267bbdd4650961cd159
ciao mondo <danielemenicucci3@gmail.com>
2024-11-28 08:13:22 +0000

    commit

--- www/ES03/es03a.php
+++ www/ES03/es03a.php
@@ -44,7 +44,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
     <h2>Registrazione Utente</h2>
-    <form action="es03a1.php" method="POST">
+    <form action="es03b.php" method="POST">
         <!-- Nome -->
         <label for="nome">Nome*:</label>
         <input type="text" id="nome" name="nome" pattern="[A-Za-zÀ-ÿ\s]+" required>
